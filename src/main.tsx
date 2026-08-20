73251f95d41e3061c46a6c8627bf6b48c8e31adc
ziherzika <tuljaandevelopment@gmail.com>
2026-08-20 04:10:07 +0200

    Load Notes workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./notes.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
